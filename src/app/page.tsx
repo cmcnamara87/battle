e10a8ef657aa8031e42f49ff24b678a81a2757f8
Craig McNamara <cmcnamara87@gmail.com>
2024-01-05 16:38:45 +1000

    go

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,166 @@
-import Image from 'next/image'
+'use client';
+import Image from 'next/image';
+import { useState } from 'react';
+
+const cards = Array.from({ length: 52 }, (_, i) => {
+  const value = (i % 13) + 1;
+  if (value < 10) {
+    return 0;
+  }
+  return value - 9;
+});
+
+const shuffledCards = cards.sort(() => Math.random() - 0.5);
+const player1Cards = shuffledCards.slice(0, shuffledCards.length / 2);
+const player2Cards = shuffledCards.slice(shuffledCards.length / 2);
 
 export default function Home() {
+  const [p1, setP1] = useState(player1Cards);
+  const [p2, setP2] = useState(player2Cards);
+
+  const [activePlayer, setActivePlayer] = useState(0);
+
+  const [stack, setStack] = useState<
+    { value: number; player: number }[]
+  >([]);
+
+  const [battle, setBattle] = useState<number>(-1);
+  const [maxBattle, setMaxBattle] = useState(0);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <div className="flex flex-row justify-center">
+        <button
+          className="bg-red-500 text-white font-bold py-2 px-4 rounded"
+          onClick={() => {
+            if (battle === 0) {
+              // battle over
+              // give all the stack to the other player
+              const winnerPlayer = activePlayer === 0 ? 1 : 0;
+              if (winnerPlayer === 0) {
+                p1.push(...stack.map((c) => c.value));
+                setP1([...p1]);
+              } else {
+                p2.push(...stack.map((c) => c.value));
+                setP2([...p2]);
+              }
+              setStack([]);
+              setActivePlayer(activePlayer === 0 ? 1 : 0);
+              setBattle(-1);
+              return;
+            }
+            let next: number | undefined;
+            if (activePlayer === 0) {
+              next = p1.shift() as number;
+              setP1([...p1]);
+            } else {
+              next = p2.shift() as number;
+              setP2([...p2]);
+            }
+            stack.unshift({
+              value: next,
+              player: activePlayer,
+            });
+
+            if (battle > 0 && next === 0) {
+              // dec battle
+              const newBattle = battle - 1;
+              setBattle(newBattle);
+              // player doesnt change
+            } else {
+              if (next > 0) {
+                setBattle(next);
+                setMaxBattle(next);
+              }
+              // not in a battle
+              setActivePlayer(activePlayer === 0 ? 1 : 0);
+            }
+          }}
+        >
+          go!
+        </button>
+      </div>
+      <div className="flex">
+        <div className="w-1/3 bg-blue-500">
+          <div>
+            YOU - {activePlayer === 0 ? 'active' : 'not active'}
+            {battle >= 0 && activePlayer === 0 && (
+              <div>
+                Battle: HP: {battle} -{' '}
+                {battle === 0 &&
+                  `You lost, they won, get they ${stack.length} monster energy`}
+                <div
+                  className="w-full h-4 bg-red-500 rounded-full animate-pulse"
+                  style={{
+                    width: `${(battle / maxBattle) * 100}%`,
+                  }}
+                ></div>
+              </div>
+            )}
+          </div>
+          <div>Monster Energy: {p1.length} cards</div>
+        </div>
+        <div className="w-1/3 flex flex-col p-11">
+          {/* {stack[0] === 0 ? 'Rest' : 'Fight'} */}
+          {/* <pre>{JSON.stringify(stack, null, 2)}</pre> */}
+          <div className="flex relative w-32 h-48">
+            {[...stack].reverse().map((card, index) => {
+              return (
+                <div key={index} className={`transform`}>
+                  <div
+                    className={`bg-${
+                      card.player === 0 ? 'blue' : 'red'
+                    }-500 w-32 h-48 flex items-center justify-center rounded-lg shadow-lg`}
+                    style={{
+                      position: 'absolute',
+                      left: `${index * 10}px`,
+                    }}
+                  >
+                    <p className="text-white text-4xl font-bold text-center">
+                      {battle >= 0 && battle !== maxBattle
+                        ? battle === 0
+                          ? `${
+                              activePlayer === 0
+                                ? 'You lost'
+                                : 'They lost'
+                            } 👊`
+                          : `${
+                              activePlayer === 0
+                                ? 'You got hit'
+                                : 'They got hit'
+                            } 👊`
+                        : card.value === 0
+                        ? 'Rest'
+                        : `Attack ${card.value}`}
+                    </p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+
+          {/* {stack[0] === 0 ? 'Rest' : 'Fight'} */}
+          {/* <pre>{JSON.stringify(stack, null, 2)}</pre> */}
+        </div>
+        <div className="w-1/3 bg-red-500">
+          {battle >= 0 && activePlayer === 1 && (
+            <div>
+              Battle: HP: {battle} -{' '}
+              {battle === 0 &&
+                `They lost, you won, you get they ${stack.length} monster energy`}
+              <div
+                className="w-full h-4 bg-blue-500 rounded-full animate-pulse"
+                style={{
+                  width: `${(battle / maxBattle) * 100}%`,
+                }}
+              ></div>
+            </div>
+          )}
+          <div className="">
+            Them - {activePlayer === 1 ? 'active' : 'not active'}
+          </div>
+          Monster Energy: {p2.length} cards
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
